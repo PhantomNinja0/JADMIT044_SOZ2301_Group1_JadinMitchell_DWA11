9a835596e0aeb1c579e5a631c0b71a783f560ae1
PhantomNinja0 <126066407+PhantomNinja0@users.noreply.github.com>
2023-06-29 15:56:24 +0200

    DWA11

---
+++ DWA11/scripts.js
@@ -0,0 +1,17 @@
+import {createStore} from './modules/store.js';
+import {reducer} from './modules/reducer.js';
+import { add, subtract, reset } from './modules/actions.js';
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+store.dispatch(add());
+store.dispatch(add());
+console.log(store.getState());
+
+store.dispatch(subtract());
+console.log(store.getState());
+
+store.dispatch(reset());
+console.log(store.getState());
